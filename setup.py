3d207a1a73d2630d937383fda602bd9127c6bb6f
Brandon Rohrer <brohrer@gmail.com>
2016-08-16 07:18:49 -0400

    Files added.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='becca_toolbox',
+    version='0.8.0',
+    description='Some tools for working with Becca',
+    url='http://github.com/brohrer/becca_toolbox',
+    download_url='https://github.com/brohrer/becca_toolbox/archive/master.zip',
+    author='Brandon Rohrer',
+    author_email='brohrer@gmail.com',
+    license='MIT',
+    packages=['becca_toolbox'],
+    include_package_data=True,
+    #install_requires=['becca_test'],
+    zip_safe=False)
