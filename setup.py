117dc8daced4c9485939201dcf7423805365057c
Brandon Rohrer <brohrer@fb.com>
2018-09-06 07:12:57 -0400

    Prep for version 10 release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='becca_toolbox',
-    version='0.10.0.0',
+    version='0.10.0',
     description='Some tools for working with Becca',
     url='http://github.com/brohrer/becca_toolbox',
     download_url='https://github.com/brohrer/becca_toolbox/archive/master.zip',
